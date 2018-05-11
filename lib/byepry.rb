3f2110d92481690462335bdb3e423fe1c7d5a047
EL KESRI Othmane <othmane.elkesri@seniormedia.fr>
2018-05-11 17:44:32 +0200

    small changes

--- lib/byepry.rb
+++ lib/byepry.rb
@@ -2,37 +2,31 @@ require 'find'
 require 'fileutils'
 require 'tempfile'
 require 'colorize'
-# require 'rubygems/command_manager'
 
 class Byepry
   def execute
     Find.find(ARGV.first) do |path|
-      if path =~ /.*\.rb$/
-        file = File.open(path, 'r+')
+      next unless path =~ /.*\.rb$/
+      file = File.open(path, 'r+')
+      # Open a temporary file
+      tmp = Tempfile.new('extract')
 
-        # Open temporary file
-        tmp = Tempfile.new("extract")
-
-        changed_file = false
-        line_number = 0
-        # Write good lines to temporary file
-        open(path, 'r').each do |l|
-          line_number += 1
-          if l.include? 'binding.pry'
-            changed_file = true
-            puts "Removed pry from File: #{path} Line: #{line_number}".green
-          end
-          tmp << l unless l.include? 'binding.pry'
+      changed_file = false
+      line_number = 0
+      # Write good lines to temporary file
+      open(path, 'r').each do |l|
+        line_number += 1
+        if l.include? 'binding.pry'
+          changed_file = true
+          puts "Removed pry from File: #{path} Line: #{line_number}".green
         end
-
-        # Close tmp, or troubles ahead
-        tmp.close
-
-        # Move temp file to origin
-        if changed_file
-          FileUtils.mv(tmp.path, path)
-        end
+        tmp << l unless l.include? 'binding.pry'
       end
+
+      # Close tmp
+      tmp.close
+      # Move temp file to origin if file changed
+      FileUtils.mv(tmp.path, path) if changed_file
     end
   end
 end
